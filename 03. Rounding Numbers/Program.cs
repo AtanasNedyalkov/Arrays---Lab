4b1609a750a303ee6be2f107ecc7fd22ae8dfcf1
Admin <Admin@DESKTOP-QFVKQ9F>
2022-06-19 16:57:45 +0300

    Add project files.

---
+++ 03. Rounding Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] numbersAsArray =
+                Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+            
+            for (int i = 0; i < numbersAsArray.Length; i++)
+            {
+                Console.WriteLine($"{numbersAsArray[i]} => {(int)Math.Round(numbersAsArray[i], MidpointRounding.AwayFromZero)}");
+            }
+            //double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            //int[] numbers = new int[nums.Length];
+
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.WriteLine($"{nums[i]} => {(int)Math.Round(nums[i], MidpointRounding.AwayFromZero)}");
+            //}
+
+        }
+    }
+}
---
+++ 6. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOfOdd = 0;
+            int sumOfEven = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 ==0)
+                {
+                    sumOfEven += numbers[i];
+                }
+                else
+                {
+                    sumOfOdd += numbers[i];
+                }
+            }
+            Console.WriteLine(sumOfEven-sumOfOdd);
+        }
+    }
+}
